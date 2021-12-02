61094f5cf9fdef8d37b91f13545f3e1a8adcd965
vincehartman38 <3585333+vincehartman38@users.noreply.github.com>
2021-12-02 13:33:54 -0500

    nonetype issue

--- agents/teamsvm.py
+++ agents/teamsvm.py
@@ -70,7 +70,7 @@ class Agent(object):
         
         # TEAM SVM CODE STARTS HERE
         covs= self.normalize_covs(new_buyer_covariates)
-        if new_buyer_embedding.all() != None:
+        if new_buyer_embedding is not None:
             vector = self.get_user_item_vectors(new_buyer_embedding)
             full_covs = np.concatenate((covs, vector))
             p, r = self.find_optimal_revenue_fast(self.trained_model_covs_and_noisy, full_covs)
