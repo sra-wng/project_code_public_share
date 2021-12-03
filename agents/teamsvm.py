af70d7ca934b6e3ee3c132b48c165f171418c975
Sara Wang <sara.yz.wang@gmail.com>
2021-12-02 21:19:48 -0500

    update with std dev for covariate normalization

--- agents/teamsvm.py
+++ agents/teamsvm.py
@@ -27,7 +27,7 @@ class Agent(object):
 
         # Training Mean and Standard Deviation for Normalization
         self.train_means = np.array([0.00534622, 0.00412864, 0.00322634])
-        self.train_stds = np.sqrt([0.86022694, 0.74355865, 0.53157464])
+        self.train_stds = np.array([0.9274842, 0.86229847, 0.72909165]) #variance = [0.86022694, 0.74355865, 0.53157464]
 
         # Item Embeddings
         self.item0_embedding = pickle.load(open("data/item0embedding", "rb"))
